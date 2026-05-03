bebdaad4ff882ac72c9b090b8d729f6344d1fe70
Fatih Güleşen <fatihgulesen96@gmail.com>
2026-05-03 00:56:23 +0000

    feat: add package model and qty details

--- models/intermediate/cc_parcel.sql
+++ models/intermediate/cc_parcel.sql
@@ -1,3 +1,13 @@
+with nb_products as (
+    SELECT
+        parcel_id
+        , count(*) as nb_models
+        , sum(quantity) as qty
+    FROM {{ ref('stg_raw__parcel_product') }}
+    GROUP BY parcel_id
+)
+
+
 SELECT
     p.*
     , EXTRACT(MONTH FROM date_purchase) as month_purchase
@@ -5,4 +15,7 @@ SELECT
     , DATE_DIFF(date_shipping, date_purchase, DAY) as expedition_time
     , DATE_DIFF(date_delivery, date_shipping, DAY) as transport_time
     , DATE_DIFF(date_delivery, date_purchase, DAY) as delivery_time
+    , IF(DATE_DIFF(date_delivery, date_purchase, DAY) > 3, (DATE_DIFF(date_delivery, date_purchase, DAY)-3), NULL) as delay
 FROM {{ ref("stg_raw__parcel") }} as p
+LEFT JOIN nb_products as nbp
+    ON nbp.parcel_id = p.parcel_id
