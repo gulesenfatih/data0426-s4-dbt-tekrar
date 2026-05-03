f2d63d0121e78121e14784678c85e8589b392934
Fatih Güleşen <fatihgulesen96@gmail.com>
2026-05-03 00:49:13 +0000

    feat: add time for different states of the parcel

--- macros/status.sql
+++ macros/status.sql
@@ -1,8 +1,8 @@
 {% macro getStatus(date_cancelled, date_delivery, date_shipping) %}
     CASE
-        WHEN {{ date_cancelled }} is not null THEN 'Cancelled'
-        WHEN {{ date_delivery }} is not null THEN 'Delivered'
-        WHEN {{ date_shipping }} is not null THEN 'Shipped'
+        WHEN {{ date_cancelled }} IS NOT NULL THEN 'Cancelled'
+        WHEN {{ date_delivery }} IS NOT NULL THEN 'Delivered'
+        WHEN {{ date_shipping }} IS NOT NULL THEN 'Shipped'
         ELSE 'In progress'
     END
 {% endmacro %}
--- models/intermediate/cc_parcel.sql
+++ models/intermediate/cc_parcel.sql
@@ -2,4 +2,7 @@ SELECT
     p.*
     , EXTRACT(MONTH FROM date_purchase) as month_purchase
     , {{ getStatus('date_cancelled', 'date_delivery', 'date_shipping') }} as status
+    , DATE_DIFF(date_shipping, date_purchase, DAY) as expedition_time
+    , DATE_DIFF(date_delivery, date_shipping, DAY) as transport_time
+    , DATE_DIFF(date_delivery, date_purchase, DAY) as delivery_time
 FROM {{ ref("stg_raw__parcel") }} as p
