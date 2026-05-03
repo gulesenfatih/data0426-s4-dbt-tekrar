31239f831f7f2c232955244df18406d955f013aa
Fatih Güleşen <fatihgulesen96@gmail.com>
2026-05-03 00:24:21 +0000

    feat: add status to cc_parcel

--- models/intermediate/cc_parcel.sql
+++ models/intermediate/cc_parcel.sql
@@ -1,4 +1,10 @@
 SELECT
     p.*
     , EXTRACT(MONTH FROM date_purchase) as month_purchase
+    , CASE 
+        WHEN date_cancelled IS NOT NULL THEN 'Cancelled'
+        WHEN date_delivery IS NOT NULL THEN 'Delivered'
+        WHEN date_shipping IS NOT NULL THEN 'Shipped'
+        ELSE 'In progress' 
+      END as status
 FROM {{ ref('stg_raw__parcel') }} as p
